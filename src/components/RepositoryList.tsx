39859dcb8a57ae16d8abadcd26fe1168b310c0a8
hidden <deyvisson.margalhaes@gmail.com>
2021-12-04 10:52:03 -0300

    adicionando o typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,16 +3,21 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-//https://api.github.com/users/Deyvi-dev/repos
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+
+}
 
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
     useEffect(() => {
         fetch('https://api.github.com/users/Deyvi-dev/repos')
             .then(response => response.json())
             .then(data => setRepositories(data))
     }, []);
-    
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
@@ -21,7 +26,7 @@ export function RepositoryList() {
                 {repositories.map(repository => {
                     return <RepositoryItem key={repository.name} repository={repository} />
                 })}
-               
+
             </ul>
         </section>
     );
